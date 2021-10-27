b18573336647667c3b278c29f0cff46b101bab6f
ambar <ambar.lee@gmail.com>
2021-10-27 21:55:53 +0800

    chore: add file arg to play.mjs

--- scripts/play.mjs
+++ scripts/play.mjs
@@ -1,20 +1,23 @@
 /* eslint-disable no-console */
+import fs from 'fs/promises'
 import path from 'path'
 import minimist from 'minimist'
 import {measure} from 'measure-bundle-size'
 
 const args = minimist(process.argv.slice(2), {
-  string: ['w', 'e'],
+  string: ['w', 'e', 'f'],
 })
 
 /**
  * ```sh
+ * DEBUG=* node play.mjs -f <file>
  * DEBUG=* node play.mjs -w <workspace> -e "import React from 'react'"
  * ```
  */
 const main = async () => {
-  const result = await measure(args.e, null, {
-    workspaceFolder: path.resolve(args.w),
+  const input = args.f ? String(await fs.readFile(args.f)) : args.e
+  const result = await measure(input, args.f, {
+    workspaceFolder: args.w && path.resolve(args.w),
     stats: 'table',
   })
   console.info(result)
