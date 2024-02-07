65517cc66821b0e60465b90770f4a6d1c7805071
ambar <ambar.lee@gmail.com>
2024-02-07 15:41:32 +0800

    chore: support stdin to scripts/play.mjs

--- scripts/play.mjs
+++ scripts/play.mjs
@@ -1,21 +1,36 @@
 /* eslint-disable no-console */
-import fs from 'fs/promises'
-import path from 'path'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import {createInterface} from 'node:readline'
 import minimist from 'minimist'
 import {measure} from 'measure-bundle-size'
 
 const args = minimist(process.argv.slice(2), {
   string: ['w', 'e', 'f'],
+  boolean: ['stdin'],
 })
+
+const getStdin = async () => {
+  const result = []
+  for await (const line of createInterface({input: process.stdin})) {
+    result.push(line)
+  }
+  return result.join('\n')
+}
 
 /**
  * ```sh
  * DEBUG=* node play.mjs -f <file>
  * DEBUG=* node play.mjs -w <workspace> -e "import React from 'react'"
+ * DEBUG=* cat <file> | node play.mjs -w <workspace> --stdin
  * ```
  */
 const main = async () => {
-  const input = args.f ? String(await fs.readFile(args.f)) : args.e
+  const input = args.f
+    ? String(await fs.readFile(args.f))
+    : args.stdin
+    ? await getStdin()
+    : args.e
   const result = await measure(input, args.f, {
     workspaceFolder: args.w && path.resolve(args.w),
     stats: 'table',
